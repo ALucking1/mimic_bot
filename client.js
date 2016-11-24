99ea1f9a28d54d47ff71a6913688fa61b1da3c0a
ManuCiao <mn.sabatino@gmail.com>
2016-11-24 16:01:14 +0000

    add Audience-speakers-board to the pool-not working

--- client.js
+++ client.js
@@ -1,5 +1,22 @@
 var ReactDOM = require('react-dom');
 var React = require('react');
-var APP = require('./components/APP');
+var Router = require('react-router');
+var Route = Router.Route;
+var DefaultRoute = Router.DefaultRoute;
 
-ReactDOM.render(<APP />, document.getElementById('react-container'));
+var APP = require('./components/APP');
+var Audience = require('./components/Audience');
+var Speaker = require('./components/Speaker');
+var Board = require('./components/Board');
+
+var routes = (
+  <Route handler={APP}>
+      <DefaultRoute handler={Audience}/>
+      <Route name="speaker" path="speaker" handler={Speaker}></Route>
+      <Route name="board" path="board" handler={Board}></Route>
+  </Route>
+);
+
+Router.run(routes, function(Handler){
+  ReactDOM.render(<Handler/>, document.getElementById('react-container'));
+});
